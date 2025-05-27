e158d094338fe297612230ca0a2809b2e9f286c8
JP Hwang <me@jphwang.com>
2025-05-27 20:14:41 +0100

    Update to use colpali

--- 50_add_to_weaviate.py
+++ 50_add_to_weaviate.py
@@ -1,25 +1,19 @@
-from helpers import IMG_DIR, WEAVIATE_COLLECTION_NAME
+from helpers import WEAVIATE_COLLECTION_NAME
 import weaviate
 from weaviate.classes.config import Property, DataType, Configure
 from weaviate.classes.init import Auth
 import base64
 from dotenv import load_dotenv
 import os
-
-# client = weaviate.connect_to_local(
-#     headers={
-#         "X-Cohere-Api-Key": os.environ["COHERE_APIKEY"]
-#     }
-# )
+from pathlib import Path
+import numpy as np
+from tqdm import tqdm
 
 load_dotenv()
 
 client = weaviate.connect_to_weaviate_cloud(
     cluster_url=os.environ["APP_WEAVIATE_CLOUD_URL"],
     auth_credentials=Auth.api_key(os.environ["APP_WEAVIATE_CLOUD_APIKEY"]),
-    headers={
-        "X-Cohere-Api-Key": os.environ["APP_COHERE_API_KEY"]
-    }
 )
 
 client.collections.delete(name=WEAVIATE_COLLECTION_NAME)
@@ -31,27 +25,36 @@ pdfs = client.collections.create(
         Property(name="image", data_type=DataType.BLOB),
     ],
     vectorizer_config=[
-        Configure.NamedVectors.multi2vec_cohere(
-            name="cohere",
-            model="embed-v4.0",
-            image_fields=["image"]
+        Configure.NamedVectors.none(
+            name="colpali",  # Uses colpali-1.3
+            vector_index_config=Configure.VectorIndex.hnsw(
+                multi_vector=Configure.VectorIndex.MultiVector.multi_vector(),
+                quantizer=Configure.VectorIndex.Quantizer.sq()
+            )
         )
     ],
-    generative_config=Configure.Generative.cohere(model="command-r"),
 )
 
 
-img_paths = list(IMG_DIR.glob("*.png"))
+embedding_dir = Path("data/embeddings")
+embedding_paths = embedding_dir.glob("*.npz")
 
 
 with pdfs.batch.fixed_size(10) as batch:
-    for img_path in img_paths:
-        batch.add_object(
-            properties={
-                "filepath": str(img_path.name),
-                "image": base64.b64encode(img_path.read_bytes()).decode("utf-8"),
-            },
-        )
+    for embedding_path in embedding_paths:
+        print(f"Importing files from: {embedding_path}")
+        embeddings = np.load(embedding_path)
+        for i, embedding in tqdm(enumerate(embeddings["embeddings"])):
+            img_filepath_str = embeddings["filepaths"][i]
+            img_path = Path(img_filepath_str)
+
+            batch.add_object(
+                properties={
+                    "filepath": img_filepath_str,
+                    "image": base64.b64encode(img_path.read_bytes()).decode("utf-8"),
+                },
+                vector={"colpali": embedding}
+            )
 
 if pdfs.batch.failed_objects:
     print(pdfs.batch.failed_objects[0].message)
--- 60_query_weaviate.py
+++ 60_query_weaviate.py
@@ -1,24 +1,17 @@
-from helpers import WEAVIATE_COLLECTION_NAME
+from helpers import WEAVIATE_COLLECTION_NAME, get_model_and_processor, text_to_colpali
 import weaviate
 from weaviate.classes.query import MetadataQuery
 from weaviate.classes.init import Auth
 from dotenv import load_dotenv
 import os
 
-# client = weaviate.connect_to_local(
-#     headers={
-#         "X-Cohere-Api-Key": os.environ["COHERE_APIKEY"]
-#     }
-# )
-
 load_dotenv()
+
+model, processor = get_model_and_processor()
 
 client = weaviate.connect_to_weaviate_cloud(
     cluster_url=os.environ["APP_WEAVIATE_CLOUD_URL"],
     auth_credentials=Auth.api_key(os.environ["APP_WEAVIATE_CLOUD_APIKEY"]),
-    headers={
-        "X-Cohere-Api-Key": os.environ["APP_COHERE_API_KEY"]
-    }
 )
 
 pdfs = client.collections.get(name=WEAVIATE_COLLECTION_NAME)
@@ -26,15 +19,16 @@ pdfs = client.collections.get(name=WEAVIATE_COLLECTION_NAME)
 queries = [
     "Diagrams of Weaviate cluster architecture, with shards, indexes and model integrations.",
     "How much does vector quantization impact memory footprint?",
-    "How do I create a new collection in Weaviate?",
-    "Vector DBs and spongebob squarepants",
+    "Can I use natural language to modify Weaviate data?",
 ]
 
 for i, query in enumerate(queries):
     print(f"Query: {queries[i]}")
-    r = pdfs.query.near_text(
-        query=query,
-        target_vector="cohere",
+    query_vector = text_to_colpali([query], model=model, processor=processor)[0]
+
+    r = pdfs.query.near_vector(
+        near_vector=query_vector,
+        target_vector="colpali",
         limit=2,
         return_metadata=MetadataQuery(distance=True)
     )
